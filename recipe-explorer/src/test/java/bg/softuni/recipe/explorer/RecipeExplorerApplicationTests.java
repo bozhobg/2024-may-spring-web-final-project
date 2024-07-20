ee005b720f27d6bb71bcba7ccfddd8562a335353
Bozho Georgiev <bozhobg@gmail.com>
2024-07-20 10:06:06 +0300

    Initial commit

---
+++ recipe-explorer/src/test/java/bg/softuni/recipe/explorer/RecipeExplorerApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.recipe.explorer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeExplorerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
